60c2bd55ff9519f8ffab5b266ad678113d495d4a
Chintan Patel <sir.chintan@gmail.com>
2016-05-06 10:57:41 +1200

    Update AVCLineTest.c
    Added InitHardware, added direction in setmotor

--- AVCLineTest.c
+++ AVCLineTest.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <time.h>
 
-
+extern "C" int InitHardWare();
 extern "C" int init(int d_lev);
 extern "C" int take_picture();
 extern "C" char get_pixel(int row,int col,int colour);
 extern "C" int Sleep(int sec, int usec);
-extern "C" int init(int d_lev);
-extern "C" int set_motor(int motor , int speed );
+extern "C" int SetMotor(int motor, int dir, int speed);
 
 int main()
 {
+    InitHardWare();
     while(1==1){
     char c;
 
@@ -18,9 +18,9 @@ int main()
         c = get_pixel(160,120,3);
 
 
-    set_motor(1,255);//sets motor connected to pin 1 to roate forward at max speed
+    SetMotor(1,1,255);//sets motor connected to pin 1 to roate forward at max speed
     Sleep(0,5000000);
-    set_motor(2,255); // sets motor connected to pin 2 to roatate forward at max speed
+    SetMotor(2,1,255); // sets motor connected to pin 2 to roatate forward at max speed
     Sleep(0,5000000);}
     return 0;
 
