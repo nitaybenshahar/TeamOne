a5e16294b85941434d023977a236f84ef72de9fe
jaytewkay <vallance.dash@gmail.com>
2016-05-07 20:04:33 +1200

    reviewed the dummy code

--- AVCLineTest.c
+++ AVCLineTest.c
@@ -22,9 +22,10 @@ int main()
     Sleep(0,5000000);
     SetMotor(2,1,255); // sets motor connected to pin 2 to roatate forward at max speed
     Sleep(0,5000000);}
-    return 0;
-
-
+    return 0; // didn't the tutor say you delete the return 0 incase it causes an issue?
+    
+    // probably want to add a [set motor speed to 0] function at the end of this so you don't have the pi just running the motor
+    // endlessly without a way to stop it when it comes time to test.
 
 
 
