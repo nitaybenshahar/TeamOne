84d354f0b0c745614e63c6626a1147991c446887
Lenton <lentonneale@gmail.com>
2016-05-30 22:35:50 +0000

    Maze turns and straight
    kinda working maybe?

--- soloMaze.cpp
+++ soloMaze.cpp
@@ -12,14 +12,14 @@ int main(){
 
     init(0);
 
-    int leftSensor;
-    int rightSensor;
+    signed int leftSensor;
+    signed int rightSensor;
     char c;
     int whiteWall;
     bool noLeftWall, noRightWall, noWallAhead;
-    int THRESHOLD = 200;
+    int THRESHOLD = 350;
     int totalWidth;
-    double leftMotor, rightMotor;
+    signed int leftMotor, rightMotor;
 
 
     while(true){
@@ -28,24 +28,25 @@ int main(){
         //returns true if there isnt a wall
 	noLeftWall =false;
 	noRightWall = false;
-	noWallAhead = false;
+	noWallAhead = true;
 
 	//get data from sensors
         leftSensor = read_analog(0);
 	rightSensor = read_analog(1);
+	printf("left sensor: %d\nright sensor: %d\n", leftSensor, rightSensor);
 	//get data from camera
-	take_picture();
+//	take_picture();
 
-	for(int i = 1; i<5; i++){
+//	for(int i = 1; i<5; i++){
 
-	    c = get_pixel(340,i*45, 3);
- 	    if(c>120){                                    //change white threshold
-            	whiteWall++;
-	    }
-    	}
-	if(whiteWall < 4){                                 //Change threshold if theres problems
-	    noWallAhead = true; //rename
-	}
+//	    c = get_pixel(340,i*40, 3);
+// 	    if(c<120){                                    //change white threshold
+//            	whiteWall++;
+//	    }
+//    	}
+//	if(whiteWall < 4){                                 //Change threshold if theres problems
+//	    noWallAhead = true; //rename
+//	}
 	
 
 	if(leftSensor<THRESHOLD){
@@ -55,40 +56,40 @@ int main(){
 	     noRightWall = true;
 	}
 	if(noRightWall){
-
-	    set_motor(1, 10);//right motor
-	    set_motor(2, -100);//left motor//CHANGE THRESHOLD
-	    Sleep(0,500000);//CHANGE THRESHOLD
+//	    Sleep(0,100000);
+	    set_motor(1, 37);//right motor
+	    set_motor(2, -60);//left motor//CHANGE THRESHOLD
+	    Sleep(0,800000);//CHANGE THRESHOLD
+	    printf("turning right");
 	}
 	else if(noWallAhead){				//stay in the center of the maze
-
-	    totalWidth = leftSensor + rightSensor;
-	    rightMotor = (leftSensor/totalWidth)*60;      //change threshold
-	    leftMotor = -(rightSensor/totalWidth)*60;
+	    rightMotor = (leftSensor/10*1.1);      //change threshold
+	    leftMotor = -(rightSensor/10);
 	    set_motor(1, rightMotor);
 	    set_motor(2,leftMotor);
 	    
-	    Sleep(0,50000);
+	    Sleep(0,1);
 	    //rotate back to centre
-	    leftMotor = -(leftSensor/totalWidth)*60;      //change threshold
-	    rightMotor = (rightSensor/totalWidth)*60;
+	    leftMotor = -(leftSensor/10);      //change threshold
+	    rightMotor = (rightSensor/10*1.1);
 	    set_motor(1, rightMotor);
 	    set_motor(2, leftMotor);
-	    
-	    Sleep(0,50000);
+	    printf("forwarsdgsdjksdgr");
+	    Sleep(0, 1);
 	}
-	else if(left){
-	    set_motor(1, 100);//right motor
-	    set_motor(2, -10);//left motor               //Change thresholds
-	    Sleep(0,500000);
+	else if(noLeftWall){
+	    set_motor(1, 60);//right motor
+	    set_motor(2, -37);//left motor               //Change thresholds
+	    Sleep(0,800000);
+	   printf("left left left left");
 	}
 	else //pop a u turn
 	{
+	   printf("pop a u turn");
 	    set_motor(1, -50);
 	    set_motor(2, -60);                            //bigger so the back doesn't hit the wall
-	    Sleep(0,500000);               		  //Change thresholds
+	    Sleep(0,100000);               		  //Change thresholds
 	}
-        Sleep(0,500000);                //TIME INTERVAL
     }
     return 0;
 }
