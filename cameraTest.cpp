c14a3f1b137e53b4fc5df028509adb0b03c1f60a
Lenton <lentonneale@gmail.com>
2016-05-31 04:11:18 +0000

    camera update

--- cameraTest.cpp
+++ cameraTest.cpp
@@ -6,9 +6,10 @@ extern "C" int init(int d_lev);
 extern "C" int take_picture();
 
 int main(){
+  char c;
   init(0);
   take_picture();
-  for(int i = 320;i>0; i++){
+  for(int i = 320;i>0; i--){
     for(int j = 0; j<240; j++){
       c = get_pixel(i,j,3);
       printf(" %d",c);
